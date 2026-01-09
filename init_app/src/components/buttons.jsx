6ebce80fd2885a983941329a6da0d371b8b73af2
yannickolinga <yannickolinga213@gmail.com>
2026-01-09 13:42:47 +0000

    header

---
+++ init_app/src/components/buttons.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        
+        <button className="primary_button" >Commencer maintenant</button>
+       <p className='secondary_button'>En savoir plus </p>
+
+    </div>
+  )
+}
